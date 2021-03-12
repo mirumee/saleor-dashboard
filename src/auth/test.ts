00f0ebb1361c2fa786afeeeb147606933d546630
Jakub Majorek <majorek.jakub@gmail.com>
2021-03-12 16:06:46 +0100

    Define node requirements, ts cleanup

---
+++ src/auth/test.ts
@@ -0,0 +1,18 @@
+// import { ApolloClient } from "apollo-client";
+
+// const AuthFactory = () => {
+//   const callbackArr = [];
+//   const token = localStorage.getItem("");
+
+//   const login = (authPlugin?: string) => {
+//     // setAuthPlugin(authPlugin);
+//   };
+
+//   const refreshToken = async (getApolloClient: () => ApolloClient<any>) => {
+//     const client = getApolloClient();
+//     // await client.mutate()
+//     // setToken
+//   };
+
+//   return {};
+// };
